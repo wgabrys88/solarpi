abc639ba8103e3ded89c08676f886f39538efe7f
Wojtek <wgabrys88@gmail.com>
2022-03-28 21:49:22 +0200

    solarpi deployer added

---
+++ solarpi_deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -v usr/local/etc/.bash_aliases /home/$USER/.bash_aliases
+
+cp -v usr/local/etc/solarpi_config.cfg /usr/local/etc/
+
+cp -v usr/local/bin/* /usr/local/bin/
+
+source /home/$USER/.bash_aliases
