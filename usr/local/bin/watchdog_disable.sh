b1160f711553e8d5cc352a4566fe13fc3eb2cc70
Wojtek <wgabrys88@gmail.com>
2022-03-29 00:34:17 +0200

    fixed watchdog enable disable

--- usr/local/bin/watchdog_disable.sh
+++ usr/local/bin/watchdog_disable.sh
@@ -4,7 +4,7 @@ source /usr/local/etc/solarpi_config.cfg
 CRONTAB_TEMP_FILE_NAME=temp_crontab
 CRONTAB_TEMP_FILE_PATH=$EXEC_CMD_WORKSPACE$CRONTAB_TEMP_FILE_NAME
 
-echo "* * * * * /usr/local/bin/ftp_cmd_file_put.sh echo" > $CRONTAB_TEMP_FILE_PATH
+echo > $CRONTAB_TEMP_FILE_PATH
 crontab $CRONTAB_TEMP_FILE_PATH
 
 rm -f $CRONTAB_TEMP_FILE_PATH
