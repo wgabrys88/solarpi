8a08eeb6953c93f52f325b0473436e4c250b76ee
Wojtek <wgabrys88@gmail.com>
2022-03-29 18:31:27 +0200

    fixed pwm off

--- usr/local/bin/pwm_off.sh
+++ usr/local/bin/pwm_off.sh
@@ -2,3 +2,11 @@
 source /usr/local/etc/pwm_config.cfg
 
 /usr/local/bin/pwm_on.sh $1 $PWM_0
+
+sleep $PWM_INIT_SLEEP
+
+if [ "$1" == "0" ]; then
+    /usr/local/bin/relay_off.sh 21
+elif [ "$1" == "1" ]; then
+    /usr/local/bin/relay_off.sh 20
+fi
