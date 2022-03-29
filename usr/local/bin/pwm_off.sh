15beea4fbacc520494de758cfc0d452c6e191ebb
Wojtek <wgabrys88@gmail.com>
2022-03-29 12:40:26 +0200

    pwm basic functionality ready

--- usr/local/bin/pwm_off.sh
+++ usr/local/bin/pwm_off.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 source /usr/local/etc/pwm_config.cfg
+
+/usr/local/bin/pwm_on.sh $1 $PWM_0
--- usr/local/bin/pwm_on.sh
+++ usr/local/bin/pwm_on.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 source /usr/local/etc/pwm_config.cfg
+
+if [ ! -d "$PWM_CHIP0/pwm$1/" ]; then
+    echo "ERROR: $PWM_CHIP0/pwm$1/ not initialized!"
+else
+    while [ ! -w "$PWM_CHIP0/pwm$1/duty_cycle" ]; do
+        sleep $PWM_INIT_SLEEP
+        echo "ERROR: $PWM_CHIP0/pwm$1/duty_cycle not writeable yet, waiting $PWM_INIT_SLEEP[s]..."
+    done
+    echo $2 > "$PWM_CHIP0/pwm$1/duty_cycle"
+fi
