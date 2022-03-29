843612504c7e6a0a8482a5e0250c5020011ab720
Wojtek <wgabrys88@gmail.com>
2022-03-29 09:06:24 +0200

    fix for deploy pwm cfg

--- solarpi_deploy.sh
+++ solarpi_deploy.sh
@@ -3,6 +3,7 @@
 cp -v usr/local/etc/.bash_aliases /home/$USER/.bash_aliases
 
 cp -v usr/local/etc/solarpi_config.cfg /usr/local/etc/
+cp -v usr/local/etc/pwm_config.cfg /usr/local/etc/
 
 cp -v usr/local/bin/* /usr/local/bin/
 
