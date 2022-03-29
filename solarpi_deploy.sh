e03a4bb0d62d98107791c9154f2fb285a16e8bb7
Wojtek <wgabrys88@gmail.com>
2022-03-29 20:46:58 +0200

    deployer added removal first

--- solarpi_deploy.sh
+++ solarpi_deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+rm -v /home/$USER/.bash_aliases
 cp -v usr/local/etc/.bash_aliases /home/$USER/.bash_aliases
-
-cp -v usr/local/etc/solarpi_config.cfg /usr/local/etc/
-cp -v usr/local/etc/pwm_config.cfg /usr/local/etc/
-
-cp -v usr/local/bin/* /usr/local/bin/
-
 source /home/$USER/.bash_aliases
+
+sudo rm -vrf /usr/local/etc/
+sudo rm -vrf /usr/local/bin/
+
+sudo cp -v usr/local/etc/* /usr/local/etc/
+sudo cp -v usr/local/bin/* /usr/local/bin/
