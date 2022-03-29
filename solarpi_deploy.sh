ea5107e3032c686ef8854827786cb03da705d7b2
Wojtek <wgabrys88@gmail.com>
2022-03-29 20:50:02 +0200

    deployer added fix for removal first

--- solarpi_deploy.sh
+++ solarpi_deploy.sh
@@ -4,8 +4,8 @@ rm -v /home/$USER/.bash_aliases
 cp -v usr/local/etc/.bash_aliases /home/$USER/.bash_aliases
 source /home/$USER/.bash_aliases
 
-sudo rm -vrf /usr/local/etc/
-sudo rm -vrf /usr/local/bin/
+sudo rm -vf /usr/local/etc/*
+sudo rm -vf /usr/local/bin/*
 
 sudo cp -v usr/local/etc/* /usr/local/etc/
 sudo cp -v usr/local/bin/* /usr/local/bin/
