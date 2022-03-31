03e8eabd81e1552cc658c4fe4199169bfe768a21
Wojtek <wgabrys88@gmail.com>
2022-03-31 22:50:28 +0200

    FTP get full path fix added

--- usr/local/bin/watchdog_job.sh
+++ usr/local/bin/watchdog_job.sh
@@ -16,13 +16,13 @@ source /usr/local/etc/solarpi_config.cfg
 
 logger "[$(date)] $0 triggered"
 
-SOLARPI_LOG_FILE_to_sent=/tmp/solarpi_workspace/upload/solpi_log
-SOLARPI_LOG_FILE_received=/tmp/solarpi_workspace/downloads/solpi_log
+SOLARPI_LOG_FILE="/tmp/solarpi_workspace/solpi_log"
 
-tail -7 /var/log/syslog >> $SOLARPI_LOG_FILE_to_sent
-/usr/local/bin/ftp_file_put.sh $SOLARPI_LOG_FILE_to_sent
+touch $SOLARPI_LOG_FILE
+echo "[$(date)] $0 ping" >> $SOLARPI_LOG_FILE
+/usr/local/bin/ftp_file_put.sh $SOLARPI_LOG_FILE
 
-/usr/local/bin/ftp_file_get.sh $SOLARPI_LOG_FILE_received
-/usr/local/bin/file_execute.sh $SOLARPI_LOG_FILE_received
+/usr/local/bin/ftp_file_get.sh "solpi_log" /tmp/solarpi_workspace/downloads
+/usr/local/bin/file_execute.sh /tmp/solarpi_workspace/downloads/solpi_log
 
 logger "[$(date)] $0 finished"
