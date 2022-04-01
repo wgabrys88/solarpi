b24df7225021804213471ee928c9273df798b2c2
Wojtek <wgabrys88@gmail.com>
2022-04-01 20:36:14 +0200

    Two way comm enabled.

--- usr/local/bin/system_pre_init.sh
+++ usr/local/bin/system_pre_init.sh
@@ -4,4 +4,5 @@ source /usr/local/etc/solarpi_config.cfg
 if [ ! -d "$SOLARPI_WORKSPACE/" ]; then
     mkdir "$SOLARPI_WORKSPACE/"
     touch "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+    touch "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
 fi
--- usr/local/bin/watchdog_job.sh
+++ usr/local/bin/watchdog_job.sh
@@ -2,15 +2,25 @@
 source /usr/local/etc/solarpi_config.cfg
 /usr/local/bin/system_pre_init.sh
 
-echo "[$(date)] $0 triggered" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
-
 if [ -d "$SOLAR_PI_HOME" ]; then
-    echo "[$(date)] $0 ping" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+    echo "[$(date)] $0 solarpi: triggered" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+    echo "[$(date)] $0 solarpi: sending PING" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
     /usr/local/bin/ftp_file_put.sh "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
 
-    /usr/local/bin/ftp_file_get.sh "$SOLARPI_LOG_FILE" "$SOLARPI_WORKSPACE/received_$SOLARPI_LOG_FILE"
+    /usr/local/bin/ftp_file_get.sh "$CLIENT_LOG_FILE" "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
+    echo "[$(date)] $0 solarpi: received file:$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE" >> \
+    "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+    echo "[$(date)] $0 solarpi: finished" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
 else
-    echo
+    echo "[$(date)] $0 client: triggered" >> "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
+    echo "[$(date)] $0 client: sending PING" >> "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
+    /usr/local/bin/ftp_file_put.sh "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
+
+    /usr/local/bin/ftp_file_get.sh "$SOLARPI_LOG_FILE" "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+    echo "[$(date)] $0 client: received file:$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE" >> \
+    "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
+    echo "[$(date)] $0 client: finished" >> "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
 fi
 
-echo "[$(date)] $0 finished" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+
+
