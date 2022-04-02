12e64f426ba5f3aafe8c3909ca59dfca726e01c6
Wojtek <wgabrys88@gmail.com>
2022-04-02 10:25:35 +0200

    Cmd send cleanup fixed

--- usr/local/bin/watchdog_job.sh
+++ usr/local/bin/watchdog_job.sh
@@ -21,12 +21,12 @@ if [ -d "$SOLAR_PI_HOME" ]; then
 else
     echo "[$(date)] $0 client: triggered" >> "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
 
-    /usr/local/bin/ftp_file_get.sh "$SOLARPI_LOG_FILE" "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+    /usr/local/bin/ftp_file_get.sh "$SOLARPI_LOG_FILE" "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE" && \
     echo "[$(date)] $0 client: received file: $SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE" >> \
     "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
 
     /usr/local/bin/ftp_file_put.sh "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
-    rm -f "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
+    echo > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
 
     /usr/local/bin/ftp_file_put.sh "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
 
