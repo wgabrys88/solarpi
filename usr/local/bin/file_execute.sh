2930adb3e33d07ee1b666b5699d1fae528cd443c
Wojtek <wgabrys88@gmail.com>
2022-04-02 09:34:59 +0200

    Receive send order fixed.

--- usr/local/bin/file_execute.sh
+++ usr/local/bin/file_execute.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-source /usr/local/etc/solarpi_config.cfg
 
 if [ -e "$1" ]; then
-    echo "[$(date)] $0 $1 solarpi: job started" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
     /bin/bash "$1"
-    echo "[$(date)] $0 $1 solarpi: job completed" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
-else
-    echo "[$(date)] $0 $1 solarpi: failed, wrong argument" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
 fi
