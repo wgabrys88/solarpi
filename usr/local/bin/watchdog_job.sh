79334f73a4051ff11e0b0b0d6425ebde1dfa0365
Wojtek <wgabrys88@gmail.com>
2022-04-03 10:09:53 +0200

    youtube as separate service

--- usr/local/bin/watchdog_job.sh
+++ usr/local/bin/watchdog_job.sh
@@ -4,16 +4,16 @@ source /usr/local/etc/solarpi_config.cfg
 
 if [ -d "$SOLAR_PI_HOME" ]; then
     echo "[$(date)] $0 solarpi: triggered" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
-    /usr/local/bin/ftp_file_get.sh "$CLIENT_LOG_FILE" "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE" && \
-    echo "[$(date)] $0 solarpi: received file: $SOLARPI_WORKSPACE/$CLIENT_LOG_FILE" >> \
-    "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+    # /usr/local/bin/ftp_file_get.sh "$CLIENT_LOG_FILE" "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE" && \
+    # echo "[$(date)] $0 solarpi: received file: $SOLARPI_WORKSPACE/$CLIENT_LOG_FILE" >> \
+    # "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
 
     /usr/local/bin/ftp_file_get_del.sh "$CLIENT_CMD_FILE" "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE" && \
     /usr/local/bin/file_execute.sh "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
     rm -f "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
 
-    yes | ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 "$SOLARPI_WORKSPACE/$CAMERA_CAPTURE_FILE"
-    /usr/local/bin/ftp_file_put.sh "$SOLARPI_WORKSPACE/$CAMERA_CAPTURE_FILE"
+    # yes | ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 "$SOLARPI_WORKSPACE/$CAMERA_CAPTURE_FILE"
+    # /usr/local/bin/ftp_file_put.sh "$SOLARPI_WORKSPACE/$CAMERA_CAPTURE_FILE"
 
     /usr/local/bin/ftp_file_put.sh "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
     echo "[$(date)] $0 solarpi: finished" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
--- usr/local/bin/youtube_streaming_enable.sh
+++ usr/local/bin/youtube_streaming_enable.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 source /usr/local/etc/solarpi_config.cfg
 
-nohup "/usr/bin/ffmpeg -re -f s16le -i /dev/zero \
--f v4l2 -thread_queue_size 512 -s 1280x720 -i /dev/video0 \
--codec:v h264 -preset veryfast -acodec aac -ab 128k -g 50 \
--f flv rtmp://x.rtmp.youtube.com/live2/$YOUTUBE_KEY" &
+/usr/bin/ffmpeg -re -f s16le -i /dev/zero -f v4l2 -thread_queue_size 512 -s 1280x720 -i /dev/video0 -codec:v h264 -preset veryfast -acodec aac -ab 128k -g 50 -f flv "rtmp://x.rtmp.youtube.com/live2/$YOUTUBE_KEY"
