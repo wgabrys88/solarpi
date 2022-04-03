1eb5bfc8e202e69be86497639d767e1d2ce01001
Wojtek <wgabrys88@gmail.com>
2022-04-03 21:19:50 +0200

    file get more logs enabled

--- usr/local/bin/ftp_file_get_del_via_dir_name.sh
+++ usr/local/bin/ftp_file_get_del_via_dir_name.sh
@@ -8,16 +8,19 @@ echo > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
 
 cmd=$(curl -n -l)
 
-echo "[$(date)] $0 Received: $cmd, converting..." >> "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
+echo "[$(date)] $0 Received: $cmd, converting..." >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
 arg1=$(echo "$cmd" | awk -F " " '{print $1}')
 arg2=$(echo "$cmd" | awk -F " " '{print $2}')
-echo "[$(date)] $0 Argument 0: $cmd, argument 1: $arg1, argument2: $arg2" >> "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
+echo "[$(date)] $0 Argument 0: $cmd, argument 1: $arg1, argument2: $arg2" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
 
 if [ "$arg1" == "stop" ] && [ "$arg2" == "youtube" ]; then
+    echo "[$(date)] $0 Egecuting: sudo systemctl $arg1 solarpi-youtube.service" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
     echo "sudo systemctl $arg1 solarpi-youtube.service" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE" && curl -n -l -Q "-RMD $cmd"
 elif [ "$arg1" == "start" ] && [ "$arg2" == "youtube" ]; then
+    echo "[$(date)] $0 Egecuting: sudo systemctl $arg1 solarpi-youtube.service" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
     echo "sudo systemctl $arg1 solarpi-youtube.service" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE" && curl -n -l -Q "-RMD $cmd"
 else
+    echo "[$(date)] $0 Executing: /usr/local/bin/$cmd.sh $arg1 $arg2" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
     echo "/usr/local/bin/$cmd.sh $arg1 $arg2" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE" && curl -n -l -Q "-RMD $cmd"
 fi
 
