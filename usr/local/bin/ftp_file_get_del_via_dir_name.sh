74c0f85cbcfd54f6a4719b16577ae2d83e97d358
Wojtek <wgabrys88@gmail.com>
2022-04-10 08:46:52 +0200

    Fixed folder command parser.

--- usr/local/bin/ftp_file_get_del_via_dir_name.sh
+++ usr/local/bin/ftp_file_get_del_via_dir_name.sh
@@ -11,7 +11,8 @@ cmd=$(curl -n -l)
 echo "[$(date)] $0 Received: $cmd, converting..." >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
 arg1=$(echo "$cmd" | awk -F " " '{print $1}')
 arg2=$(echo "$cmd" | awk -F " " '{print $2}')
-echo "[$(date)] $0 Argument 0: $cmd, argument 1: $arg1, argument2: $arg2" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+arg3=$(echo "$cmd" | awk -F " " '{print $3}')
+echo "[$(date)] $0 Arg 0: $cmd, arg 1: $arg1, arg 2: $arg2, arg 3: $arg3" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
 
 # Stop youtube service
 if [ "$arg1" == "stop" ] && [ "$arg2" == "youtube" ]; then
@@ -22,24 +23,20 @@ elif [ "$arg1" == "start" ] && [ "$arg2" == "youtube" ]; then
     echo "[$(date)] $0 Egecuting: sudo systemctl $arg1 solarpi-youtube.service" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
     echo "sudo systemctl $arg1 solarpi-youtube.service" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
 
-# Motor right slow forward
+# Motor: right, speed: slow, direction: forward
 elif [ "$arg1" == "right" ] && [ "$arg2" == "forward" ]; then
-    echo "/usr/local/bin/motor_start.sh 0 1423000 $arg3" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
-# Motor right slow backward
+    echo "/usr/local/bin/motor_start.sh 0 1513000 $arg3" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
+# ... direction: backward
 elif [ "$arg1" == "right" ] && [ "$arg2" == "backward" ]; then
-    echo "/usr/local/bin/motor_start.sh 0 1623000 $arg3" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
+    echo "/usr/local/bin/motor_start.sh 0 1533000 $arg3" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
 
-# Motor left slow forward
+# Motor: left, speed: slow, direction: forward
 elif [ "$arg1" == "left" ] && [ "$arg2" == "forward" ]; then
-    echo "sudo /usr/local/bin/motor_start.sh 1 1623000 $arg3" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
-# Motor left slow backward
+    echo "/usr/local/bin/motor_start.sh 1 1533000 $arg3" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
+# ... direction: backward
 elif [ "$arg1" == "left" ] && [ "$arg2" == "backward" ]; then
-    echo "/usr/local/bin/motor_start.sh 1 1423000 $arg3" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
+    echo "/usr/local/bin/motor_start.sh 1 1513000 $arg3" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
 
-# Run motor
-# elif [ "$arg1" == "motor" ]; then
-#     echo "[$(date)] $0 Executing: /usr/local/bin/motor_start.sh $arg1 $arg2 $arg3" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
-#     echo "/usr/local/bin/motor_start.sh $arg1 $arg2 $arg3" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
 # Run command
 else
     echo "[$(date)] $0 Executing: /usr/local/bin/$cmd.sh $arg1 $arg2" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
