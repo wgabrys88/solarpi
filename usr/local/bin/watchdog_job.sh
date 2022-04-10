4ef808e7e9c86e8ddb05bdc8a06734ea859ea530
Wojtek <wgabrys88@gmail.com>
2022-04-10 21:01:22 +0200

    USB enabled by default and logging disabled by default.

--- usr/local/bin/watchdog_job.sh
+++ usr/local/bin/watchdog_job.sh
@@ -12,12 +12,12 @@ if [ -d "$SOLAR_PI_HOME" ]; then
     /usr/local/bin/file_execute.sh "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
     echo > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
 
-    /usr/bin/systemctl status solarpi-youtube.service | /usr/bin/head -n3 >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+    # /usr/bin/systemctl status solarpi-youtube.service | /usr/bin/head -n3 >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
 
     # yes | ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 "$SOLARPI_WORKSPACE/$CAMERA_CAPTURE_FILE"
     # /usr/local/bin/ftp_file_put.sh "$SOLARPI_WORKSPACE/$CAMERA_CAPTURE_FILE"
 
-    /usr/local/bin/ftp_file_put.sh "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
+    # /usr/local/bin/ftp_file_put.sh "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
     echo "[$(date)] $0 solarpi: finished" >> "$SOLARPI_WORKSPACE/$SOLARPI_LOG_FILE"
 else
     echo "[$(date)] $0 client: triggered" >> "$SOLARPI_WORKSPACE/$CLIENT_LOG_FILE"
