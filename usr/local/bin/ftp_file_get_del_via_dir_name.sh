d924791dbcd50cd898be7c1c88ed2818d6fbf20c
Wojtek <wgabrys88@gmail.com>
2022-04-16 19:22:12 +0200

    added admin console and fixed init process

--- usr/local/bin/ftp_file_get_del_via_dir_name.sh
+++ usr/local/bin/ftp_file_get_del_via_dir_name.sh
@@ -35,8 +35,14 @@ elif [ "$arg1" == "left" ] && [ "$arg2" == "forward" ]; then
 elif [ "$arg1" == "left" ] && [ "$arg2" == "backward" ]; then
     echo "/usr/local/bin/motor_start.sh 1 1513000 $arg3" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
 
+# ADMIN
+elif [ "$arg1" == "admin" ]; then
+    echo "$arg2" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
+
+# Reboot
 elif [ "$cmd" == "reboot" ]; then
     echo "sudo reboot" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
+# Shutdown Now
 elif [ "$cmd" == "shutdown" ]; then
     echo "sudo shutdown now" > "$SOLARPI_WORKSPACE/$CLIENT_CMD_FILE"
 
