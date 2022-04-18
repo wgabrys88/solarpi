3d42ace418ad76c0489cc0e00f9211e1b524a95e
Wojtek <wgabrys88@gmail.com>
2022-04-18 14:49:10 +0200

    init bug fixed

--- usr/local/bin/relay_init.sh
+++ usr/local/bin/relay_init.sh
@@ -3,6 +3,7 @@ source /usr/local/etc/solarpi_config.cfg
 
 if [ ! -d "$GPIO_DIR_PATH""gpio2$1/" ]; then
     echo "2$1" > /sys/class/gpio/export
+    echo out > "/sys/class/gpio/gpio2$1/direction"
     echo "$GPIO_DIR_PATH""gpio2$1/ init completed"
 else
     echo "ERROR: $GPIO_DIR_PATH""gpio2$1/ already exported"
