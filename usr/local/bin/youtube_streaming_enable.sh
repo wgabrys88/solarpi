cad6ee4befefe69990573a04079b5e8a38f15e91
wgabrys88 <40343971+wgabrys88@users.noreply.github.com>
2022-04-20 00:31:54 +0200

    improved stream stability

--- usr/local/bin/youtube_streaming_enable.sh
+++ usr/local/bin/youtube_streaming_enable.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /usr/local/etc/solarpi_config.cfg
 
-/usr/bin/ffmpeg -re -f s16le -i /dev/zero -f v4l2 -thread_queue_size 512 -s 1280x720 -i /dev/video0 -codec:v h264_omx -preset veryfast -acodec aac -ab 128k -g 50 -f flv "rtmp://x.rtmp.youtube.com/live2/$YOUTUBE_KEY"
+/usr/bin/ffmpeg -hide_banner -re -f s16le -i /dev/zero -f v4l2 -thread_queue_size 512 -s 1280x720 -i /dev/video0 -codec:v h264_omx -acodec aac -ab 128k -f flv "rtmp://x.rtmp.youtube.com/live2/$YOUTUBE_KEY"
